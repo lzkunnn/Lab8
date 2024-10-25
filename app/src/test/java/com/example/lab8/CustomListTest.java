31af95bd7660f92bf0276ddfcd556642b7665cc5
Zhikun Liu <liu06249@gmail.com>
2024-10-25 02:39:35 -0600

    Add failing test for addCity method in CustomList

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+
+    public CustomList MockCityList() {
+        CustomList list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest() {
+        CustomList list = MockCityList();
+        int expectedCountBeforeAdd = 0;
+        assertEquals(expectedCountBeforeAdd, list.getCount(), "count should have been zero");
+        City cityToAdd = new City("Nanton", "AB");
+        list.addCity(cityToAdd);
+
+        int expectedCountAfterAdd = 1;
+        assertEquals(expectedCountAfterAdd, list.getCount(), "The city count is now 1 after adding one city");
+
+    }
+
+
 
 
 }
